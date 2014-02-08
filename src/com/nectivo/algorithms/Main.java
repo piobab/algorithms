05fa7851172293462d218627d0c13cc1fd261cd3
Piotr Babel <piotr.babel@gmail.com>
2014-02-08 11:37:20 +0100

    Initial commit.

---
+++ src/com/nectivo/algorithms/Main.java
@@ -0,0 +1,11 @@
+package com.nectivo.algorithms;
+
+/**
+ * Created by pbabel on 08.02.2014.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Main method");
+    }
+}
