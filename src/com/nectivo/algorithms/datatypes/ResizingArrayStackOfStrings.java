af1a807a8b5d5205f0c13cc2cda908877eb9d423
Piotr Babel <piotr.babel@gmail.com>
2014-05-03 10:28:32 +0200

    Stack implementation with resizing array.

---
+++ src/com/nectivo/algorithms/datatypes/ResizingArrayStackOfStrings.java
@@ -0,0 +1,37 @@
+package com.nectivo.algorithms.datatypes;
+
+/**
+ * Created by piobab on 03.05.2014.
+ */
+public class ResizingArrayStackOfStrings {
+    private String[] s;
+    private int N;
+
+    public ResizingArrayStackOfStrings() {
+        s = new String[1];
+    }
+
+    public void push(String item) {
+        if (N == s.length) {
+            resize(2 * s.length);
+        }
+        s[N++] = item;
+    }
+
+    private void resize(int capacity) {
+        String[] copy = new String[capacity];
+        for (int i = 0; i < N; i++) {
+            copy[i] = s[i];
+        }
+        s = copy;
+    }
+
+    public String pop() {
+        String item = s[--N];
+        s[N] = null;
+        if (N > 0 && N == s.length / 4) {
+            resize(s.length / 2);
+        }
+        return item;
+    }
+}
---
+++ test/com/nectivo/algorithms/searching/QuickFindUFTest.java
@@ -0,0 +1,30 @@
+package com.nectivo.algorithms.searching;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by piobab on 27.03.2014.
+ */
+public class QuickFindUFTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testConnected() throws Exception {
+
+    }
+
+    @Test
+    public void testUnion() throws Exception {
+
+    }
+}
