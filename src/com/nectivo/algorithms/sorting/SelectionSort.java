bdac7106a6fd1c5a04c1b50d4b8c2a7f8779d6ef
Piotr Babel <piotr.babel@gmail.com>
2014-05-04 14:10:09 +0200

    Simple implementation of selection sort.

---
+++ src/com/nectivo/algorithms/sorting/SelectionSort.java
@@ -0,0 +1,32 @@
+package com.nectivo.algorithms.sorting;
+
+/**
+ * Created by piobab on 04.05.2014.
+ * <p/>
+ * This implementation has quadratic time of execution.
+ */
+public class SelectionSort {
+
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        for (int i = 0; i < N; i++) {
+            int min = i;
+            for (int j = i + 1; j < N; j++) {
+                if (less(a[j], a[min])) {
+                    min = j;
+                }
+            }
+            exch(a, i, min);
+        }
+    }
+
+    public static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    public static void exch(Comparable[] a, int i, int j) {
+        Comparable swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+}
