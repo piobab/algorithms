90ea6dd03c4266e9a061b57f61fcc2d2e808438e
Piotr Babel <piotr.babel@gmail.com>
2014-05-27 11:46:10 +0200

    Add more methods for BST operation.

--- src/com/nectivo/algorithms/datatypes/BinarySearchTree.java
+++ src/com/nectivo/algorithms/datatypes/BinarySearchTree.java
@@ -11,6 +11,7 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
         private Key key;
         private Value value;
         private Node left, right;
+        private int count;
 
         public Node(Key key, Value value) {
             this.key = key;
@@ -23,18 +24,18 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
     }
 
     private Node put(Node x, Key key, Value value) {
-        if(x == null) {
+        if (x == null) {
             return new Node(key, value);
         }
         int cmp = key.compareTo(x.key);
-        if(cmp < 0) {
+        if (cmp < 0) {
             x.left = put(x.left, key, value);
-        } else if(cmp > 0) {
+        } else if (cmp > 0) {
             x.right = put(x.right, key, value);
         } else {
             x.value = value;
         }
-
+        x.count = 1 + size(x.left) + size(x.right);
         return x;
     }
 
@@ -59,11 +60,118 @@ public class BinarySearchTree<Key extends Comparable<Key>, Value> {
         return null;
     }
 
-    public void delete(Key key) {
-
+    public Key floor(Key key) {
+        Node x = floor(root, key);
+        if (x == null) {
+            return null;
+        }
+        return x.key;
     }
 
-    public Iterable<Key> iterator() {
-        return null;
+    private Node floor(Node x, Key key) {
+        if (x == null) {
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+
+        if (cmp == 0) {
+            return x;
+        }
+
+        if (cmp < 0) {
+            return floor(x.left, key);
+        }
+
+        Node t = floor(x.right, key);
+        if (t != null) {
+            return t;
+        } else {
+            return x;
+        }
+    }
+
+    public int size() {
+        return size(root);
+    }
+
+    private int size(Node x) {
+        if (x == null) {
+            return 0;
+        }
+        return x.count;
+    }
+
+    public int rank(Key key) {
+        return rank(key, root);
+    }
+
+    private int rank(Key key, Node x) {
+        if (x == null) {
+            return 0;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            return rank(key, x.left);
+        } else if (cmp > 0) {
+            return 1 + size(x.left) + rank(key, x.right);
+        } else {
+            return size(x.left);
+        }
+    }
+
+    public void delete(Key key) {
+        root = delete(root, key);
+    }
+
+    private Node delete(Node x, Key key) {
+        if(x == null) {
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+        if(cmp < 0) {
+            // search for key
+            x.left = delete(x.left, key);
+        } else if(cmp > 0) {
+            // search for key
+            x.right = delete(x.right, key);
+        } else {
+            // no right child
+            if(x.right == null) {
+                return x.left;
+            }
+            // no left child
+            if(x.left == null) {
+                return x.right;
+            }
+
+            Node t = x;
+            // find successor
+            x = min(t.right);
+            x.right = deleteMin(t.right);
+            x.left = t.left;
+        }
+        // update subtree counts
+        x.count = size(x.left) + size(x.right) + 1;
+        return x;
+    }
+
+    private Node min(Node x) {
+        if(x.left == null) {
+            return x;
+        }
+        return min(x.left);
+    }
+
+    public void deleteMin() {
+        root = deleteMin(root);
+    }
+
+    private Node deleteMin(Node x) {
+        if (x.left == null) {
+            return x.right;
+        }
+        x.left = deleteMin(x.left);
+        x.count = 1 + size(x.left) + size(x.right);
+        return x;
     }
 }
